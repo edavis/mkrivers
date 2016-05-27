4261a8b0555afa20b53639786374e6bb0929691a
Eric Davis <edavis@eresources.com>
2016-05-27 08:56:04 -0700

    Make URL log lines cleaner

--- mkrivers.py
+++ mkrivers.py
@@ -243,7 +243,8 @@ class WebFeed(object):
             return deque(maxlen=RIVER_UPDATES_LIMIT)
 
     def log(self, msg, level='debug'):
-        msg = ('[%s:%-50s] ' % (path_basename(self.source.fname), self.url[:50])) + msg
+        prefix = '(%s) %s' % (path_basename(self.source.fname, self.url))
+        msg = ('[%-50s] ' % prefix[:50]) + msg
         func = getattr(logging, level)
         func(msg)
 
